80c9a873b61048ef2013a1fd319d51bbcbe9f438
mlakhouadri <81520469+mlakhouadri@users.noreply.github.com>
2021-03-29 15:00:49 +0200

    Update Main.java

--- Ka-Ching/src/at/ac/fhcampuswien/Main.java
+++ Ka-Ching/src/at/ac/fhcampuswien/Main.java
@@ -5,22 +5,19 @@ public class Main {
     public static void main(String[] args) {
         int number = 1;
 
-        while (number <= 20) {
-            if (number % 3 == 0) {
-                System.out.println("ka");
+        while (number <= 50) {
+            if (number % 15 == 0) {
+                System.out.println("ka-ching");
             } else if (number % 5 == 0) {
                 System.out.println("ching");
-            } else if (number % 15 == 0) {
-                System.out.println("ka-ching");
+            } else if (number % 3 == 0) {
+                System.out.println("ka");
             } else {
                 System.out.println(number);
             }
-
             number++;
         }
     }
-
-
 }
 
 
